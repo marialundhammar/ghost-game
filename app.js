4b5f1ce7d790626b29e6b10008288999792af979
Maria Lundhammar <marialundhammar@gmail.com>
2022-03-14 14:32:25 +0100

    added connection and some other stuff

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("Hello world");
+const express = require('express');
+const cors = require('cors');
+const logger = require('morgan');
+
+// express
+const app = express();
+
+// middlewares
+app.use(logger('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routes
+app.use(require('./routes'));
+
+// serve static files from public-folder
+app.use(express.static('public'));
+
+module.exports = app;
