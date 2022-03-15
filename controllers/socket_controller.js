ef09193f6847bd52496fc2d7267e1a47a13ba30a
Maria Lundhammar <marialundhammar@gmail.com>
2022-03-15 14:57:49 +0100

    changed functions in socket_controller

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -7,36 +7,48 @@ const debug = require('debug')('chat:socket_controller');
 // list of socket-ids and their username
 const player = {};
 
-module.exports = function (socket) {
-    debug('a new client has connected', socket.id);
+let io = null;
 
-    // handle user disconnect
-    socket.on('disconnect', function () {
-        debug(`Client ${socket.id} disconnected :(`);
 
-        // let everyone connected know that user has disconnected
-        this.broadcast.emit('player:disconnected', player[socket.id]);
+const handleUserJoined = function (playername, callback) {
+    // associate socket id with playername
+    playername[this.id] = playername;
 
-        // remove user from list of connected users
-        delete player[socket.id];
+    debug(`Player ${playername} with socket id ${this.id} joined`);
+
+    // let everyone know that someone has connected to the game
+    this.broadcast.emit('user:connected', playername);
+
+    // confirm join
+    callback({
+        success: true,
     });
+
+}
+
+const handleDisconnect = function () {
+    debug(`Client ${this.id} disconnected :(`);
+
+    // let everyone connected know that user has disconnected
+    this.broadcast.emit('player:disconnected', player[this.id]);
+    console.log("hej från disconnect")
+
+    // remove user from list of connected users
+    delete player[this.id];
+}
+
+
+module.exports = function (socket, _io) {
+    debug('a new client has connected', socket.id);
+    io = _io;
+
+    io.emit("new-connection", "A new user connected");
+    // handle user disconnect
+    socket.on('disconnect', handleDisconnect);
 
 
     // handle user joined
-    socket.on('player:joined', function (playername, callback) {
-        // associate socket id with username
-        playername[socket.id] = playername;
-
-        debug(`Player ${playername} with socket id ${socket.id} joined`);
-
-        // let everyone know that someone has connected to the chat
-        socket.broadcast.emit('user:connected', playername);
-
-        // confirm join
-        callback({
-            success: true,
-        });
-    });
+    socket.on('player:joined', handleUserJoined);
 
 
 
